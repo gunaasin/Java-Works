3084d3282677bfd42abecae867e1be5ec547eba0
gunaasin <guna.asin06@gmail.com>
2024-07-31 21:54:01 +0530

    day2

---
+++ 6 months/Day 2/Print.java
@@ -0,0 +1,14 @@
+
+public class Print{
+    public static void main(String[] args)
+    {
+        int i;
+        int j;
+        for(i=1;i<=10;i++){
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
