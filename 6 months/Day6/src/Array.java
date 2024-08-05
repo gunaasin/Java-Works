ec922ea2d6ecdbded5a9e77bb21a9e5af58218e6
gunaasin <guna.asin06@gmail.com>
2024-08-05 19:15:02 +0530

    day 6

--- 6 months/Day6/src/Array.java
+++ 6 months/Day6/src/Array.java
@@ -2,27 +2,17 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
-         System.out.print("Enter the Number of train :");
-         int train = scan.nextInt();
-         int[][] a = new int[train][];
-
-        for(int i = 0; i < train; i++){
-            System.out.print("Enter no of comportment in "+i+" train:");
-            a[i] = new int[scan.nextInt()];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number of std : ");
+        int n = sc.nextInt();
+        int[] a= new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter "+(i+1)+": Student age  ");
+            a[i] = sc.nextInt();
         }
-
-         for (int i = 0; i < a.length; i++) {
-             for (int j = 0; j < a[i].length; j++) {
-                 System.out.print("Enter the " + i + " train ," + j+ " comportment How many Seets :");
-                 a[i][j] = scan.nextInt();
-             }System.out.println();
-         }
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j <a[i].length; j++) {
-                System.out.print(" "+ a[i][j]+" ");
-            }System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i]+ " ");
         }
     }
 }
+
