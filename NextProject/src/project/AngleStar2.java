dad35059181217f9a3b37d3d838013e00b9293c4
gunaasin <guna.asin06@gmail.com>
2024-08-10 17:27:16 +0530

    Day 11

--- NextProject/src/project/AngleStar2.java
+++ NextProject/src/project/AngleStar2.java
@@ -1,20 +1,22 @@
 package project;
+
 import java.util.Scanner;
 
-public class AngleStar {
+public class AngleStar2 {
 
 	public static void main(String[] args) {
-		
-		
 		Scanner scan = new Scanner(System.in);
+		
 		int n = scan.nextInt();
 		int i,j;
 		for(i=1;i<=n;i++) {
+			for(j=1;j<=n-i;j++) {
+				System.out.print("  ");
+			}
 			for(j=1;j<=(2*i)-1;j++) {
 				System.out.print("* ");
 			}System.out.println();
 		}
-
 	}
-
 }
+ 
