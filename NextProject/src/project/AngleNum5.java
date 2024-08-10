05c22cf026dce87ed345114efb9ecdeb7d34cc2f
gunaasin <guna.asin06@gmail.com>
2024-08-10 18:16:22 +0530

    Day 11

---
+++ NextProject/src/project/AngleNum5.java
@@ -0,0 +1,29 @@
+package project;
+
+import java.util.Scanner;
+
+public class AngleNum5 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int i,j;
+		
+		for(i=1;i<=n;i++) {
+			int count = 1;
+			for(j=1;j<=n-i;j++) {
+				System.out.print("  ");
+			}
+			
+			for(j=1;j<=(2*i)-1;j++) {
+				if(j<i) {
+					System.out.print(count++ + " ");
+				}
+				else {
+					System.out.print(count-- + " ");
+				}
+				
+			}System.out.println();
+		}
+
+	}
+}
