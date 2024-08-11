bf3a82de8da9dc48a9dff5bd651ebc080ec64df1
gunaasin <guna.asin06@gmail.com>
2024-08-11 08:01:14 +0530

    Day 12

---
+++ Day12/src/day12/AngleNum.java
@@ -0,0 +1,24 @@
+package day12;
+
+import java.util.Scanner;
+
+public class AngleNum {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int i,j;
+		int num = (n-n) + 1;
+			for(i=1 ; i<=n ; i++) {
+				for(j=1;j<=i;j++) {
+					if(num<10) {
+						System.out.print("0"+ num + " ");
+					}else {
+						System.out.print(num + " ");
+					}
+					
+					num++;
+				}System.out.println();
+			}
+	}
+}
