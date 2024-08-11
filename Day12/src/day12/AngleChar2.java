7cfdcbd7c42c9a937ed2ae618e6b20a5c7c5b796
gunaasin <guna.asin06@gmail.com>
2024-08-11 14:00:59 +0530

    Day 12

--- Day12/src/day12/AngleChar2.java
+++ Day12/src/day12/AngleChar2.java
@@ -2,19 +2,19 @@ package day12;
 
 import java.util.Scanner;
 
-public class AngleChar {
+public class AngleChar2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			Scanner scan = new Scanner(System.in);
 			int n = scan.nextInt();
-			char cha = 'A';
+			char ch = 'A';
+			
 			for(int i =1;i<=n;i++) {
-				for(int j =1;j<=i;j++) {
-					System.out.print(cha + " ");
-				}
-				System.out.println();
-				cha++;
+				for(int j = 1;j<=i;j++) {
+					System.out.print(ch++ + " " );
+				}System.out.println();
 			}
 	}
+
 }
