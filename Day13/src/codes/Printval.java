1e7cf61fd8d761f7cbc6e7c4415770eb596fcc64
gunaasin <guna.asin06@gmail.com>
2024-08-12 16:51:42 +0530

    Day 13

---
+++ Day13/src/codes/Printval.java
@@ -0,0 +1,43 @@
+package codes;
+
+import java.util.Scanner;
+
+public class Printval {
+	
+static String angryProfesser(int k,int[] a)
+	{
+		int count = 0;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]<=0)
+			{
+				count++;
+			}	
+		}
+		if(count >=k) {
+			return "NO";
+		}else {
+			return "YES";
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner scan = new Scanner(System.in);
+	
+	int n = scan.nextInt();
+	int k = scan.nextInt();
+
+	int[] a = new int[n];
+	
+	for(int i=0;i<a.length;i++)
+	{
+		a[i] = scan.nextInt();
+	}
+	
+	System.out.println(angryProfesser(k , a));
+				
+	}
+
+}
