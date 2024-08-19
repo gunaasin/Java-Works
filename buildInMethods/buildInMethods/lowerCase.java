0f729b90e0d775f2e240818f34ae690b11fb0d30
gunaasin <guna.asin06@gmail.com>
2024-08-19 22:35:32 +0530

    Day15

--- buildInMethods/buildInMethods/lowerCase.java
+++ buildInMethods/buildInMethods/lowerCase.java
@@ -2,11 +2,14 @@ package buildInMethods;
 import java.util.Scanner;
 
 public class lowerCase {
-	static String toLowerCase(String n) {
+	static String toLowerCase(String n) 
+	{
 		String t = "";
-		for (int i =0;i<n.length();i++) {
+		for (int i =0;i<n.length();i++)
+		{
 			char c = n.charAt(i);
-			if(c>='A'&& c<='Z') {
+			if(c>='A'&& c<='Z')
+			{
 				t = t + (char)(c+32);
 			}else {
 				t = t + (char)(c);
@@ -16,9 +19,11 @@ public class lowerCase {
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner s = new Scanner(System.in);
 		String n = s.nextLine();
+		
 		System.out.println(toLowerCase(n));
 	}
 
