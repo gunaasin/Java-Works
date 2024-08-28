f62fea3bc383600f3d72e97c87003d05aa8cbde9
gunaasin <guna.asin06@gmail.com>
2024-08-28 08:57:52 +0530

    day 22

--- Day22/printOddIndex.java
+++ Day22/printOddIndex.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class printOddIndex {
-
-	
 	public static void PrintArray(int[] arr) {
 		for(int i=2;i<arr.length;i+=2) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	
+	public static void PrintArray2(int[] arr) { // print reverse order in the array
+		for(int i=arr.length-1;i>=0;i--) {
 			System.out.print(arr[i] + " ");
 		}
 	}
@@ -19,7 +23,10 @@ public class printOddIndex {
 			arr[i]=scan.nextInt();
 		}
 		
-		PrintArray(arr);
+		System.out.println(arr);
+		
+//		PrintArray(arr);
+		PrintArray2(arr);
 
 	}
 
