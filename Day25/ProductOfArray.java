0f9ec3cdae3cf58b00d9a7bd53447949b67c1809
gunaasin <guna.asin06@gmail.com>
2024-09-01 17:12:28 +0530

    Day25

---
+++ Day25/ProductOfArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ProductOfArray {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		int n= scan.nextInt();
+		
+		int[] ar = new int[n];
+		
+		for(int i=0;i<ar.length;i++) {
+			ar[i] = scan.nextInt();
+		}
+		
+		
+		int product =1;
+		
+		for(int i=0;i<ar.length;i++) {
+			product = product*ar[i];
+		}
+		
+		
+		System.out.println(product);
+	}
+}
