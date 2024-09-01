4d90799784e2b844ff96bb61717771d5a097f3c7
gunaasin <guna.asin06@gmail.com>
2024-09-01 18:07:23 +0530

    Day25

--- Day25/largestNumOfArray.java
+++ Day25/largestNumOfArray.java
@@ -1,30 +1,45 @@
 import java.util.Scanner;
 
-public class largestNumOfArray {
+public class largestNumOfArray 
+{
 
-	public static void findVal(int[] arr) {
-//		int res = arr[0];
-		int res = Integer.MIN_VALUE;
-		for(int i =1;i<arr.length;i++) {
-			int t = res;
-			res = t<arr[i] ? arr[i] : res; 
+	
+	public static void findVal(int[] arr)
+	{
+		int res = arr[0];
+//		int res = Integer.MIN_VALUE;	
+		for(int i =1;i<arr.length;i++)
+		{
+			res = res<arr[i] ? arr[i] : res; 
 		}
-		System.out.println(res);
+		System.out.println("The max value "+res);
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void findVal2(int[] arr)
+	{
+		int res = arr[0];
+//		int res = Integer.MIN_VALUE;	
+		for(int i =1;i<arr.length;i++)
+		{
+			res = res<arr[i] ?  res:arr[i] ; 
+		}
+		System.out.println("The min value "+res);
+	}
+	
+	
+	public static void main(String[] args) 
+	{
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		
 		int[] arr = new int[n];
-		
-		for(int i =0;i<arr.length;i++) {
+		for(int i =0;i<arr.length;i++) 
+		{
 			arr[i]=scan.nextInt();
-		}
-		
-		
+	    }
 		findVal(arr);
+		findVal2(arr);
+		
 
 	}
 
