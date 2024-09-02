7b2937126491a21a9152e55c8e2d46f76eea4fa1
gunaasin <guna.asin06@gmail.com>
2024-09-02 08:57:50 +0530

    Day27

---
+++ Day27/secondLargestElementinArray.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class secondLargestElementinArray {
+	
+	public static void findArray(int[] arr) {
+		int max =arr[0];
+		int secondMax = Integer.MIN_VALUE;
+		for (int i = 1; i < arr.length; i++) {
+			if(arr[i]>max ) {
+				secondMax =max;
+				max = arr[i];
+			}else if(secondMax<arr[i]) {
+				secondMax =arr[i];
+			}
+
+	
+		}
+		
+//		System.out.println(max);
+
+		System.out.println("The second largest element in the array "+secondMax);
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+			
+		}
+		
+		findArray(arr);
+		
+
+	}
+
+}
