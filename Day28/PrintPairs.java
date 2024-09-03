1189a7302a721f836b84ce4b8adb07d70aa3a345
gunaasin <guna.asin06@gmail.com>
2024-09-03 13:56:21 +0530

    Day28

---
+++ Day28/PrintPairs.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrintPairs {
+	
+	public static void printAns(int[] arr) 
+	{
+		for (int i = 0; i < arr.length-1; i++) // arr.length-1 is define by total length -1 because 
+		{
+			for (int j = i+1; j < arr.length; j++) 
+			{
+				System.out.println(arr[i] + " , " + arr[j]);
+			}
+		}
+	}
+	
+	
+	
+
+	public static void main(String[] args) 
+	{
+		Scanner scan =new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			arr[i] =scan.nextInt();
+		}
+		
+		printAns(arr);
+
+	}
+
+}
