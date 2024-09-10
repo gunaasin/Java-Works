23adfaa53b11580ccbe543f9ed93329986876194
gunaasin <guna.asin06@gmail.com>
2024-09-10 14:13:38 +0530

    Day 32

---
+++ Day32/subArraySum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class subArraySum {
+	
+	public static void findSubArraySum(int[] arr,int k) {
+		for (int i = 0; i <= arr.length-k; i++) {
+			int sum =0;
+			for (int j = i; j < i+k; j++) {
+				sum = sum + arr[j];
+				System.out.print(arr[j] + " ");
+			}
+			System.out.println("- "+sum);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n =scan.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=scan.nextInt();
+		}
+		int k = scan.nextInt();
+		
+	findSubArraySum(arr, k);
+
+	}
+
+}
