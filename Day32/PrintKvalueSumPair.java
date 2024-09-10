57c33053e551b91c5cb5904aae2ec35013406f66
gunaasin <guna.asin06@gmail.com>
2024-09-10 15:05:26 +0530

    Day 32

--- Day32/PrintKvalueSumPair.java
+++ Day32/PrintKvalueSumPair.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
-public class CountOfGivenElementinSubArray {
-
-	public static int COGEISA(int[] arr , int k, int s){
+public class PrintKvalueSumPair {
+	public static void COGEISA(int[] arr , int k, int s){
 		int count =0;
 		for (int i = 0; i <=arr.length-k; i++) {
 			int sum =0;
 			for (int j = i; j <i+k; j++) {
 				sum +=arr[j];
-//				System.out.print(arr[j]);
+
 			}
-//			System.out.println();
+
 			if(sum ==s) {
-				count++;
+				for (int j = i; j <i+k; j++) {
+					
+					System.out.print(arr[j] + " ");
+				}
+				System.out.println();
 			}
 		}
-		return count;
+		
 	}
-	
-	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n =scan.nextInt();
@@ -28,7 +29,8 @@ public class CountOfGivenElementinSubArray {
 		}
 		int k = scan.nextInt();
 		int s = scan.nextInt();
-		System.out.println(COGEISA(arr,k,s));
+		COGEISA(arr, k, s);
+
 	}
 
 }
