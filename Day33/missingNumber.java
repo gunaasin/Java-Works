e78936e9a7a165940e17af3ce953f7154cc6ca3b
gunaasin <guna.asin06@gmail.com>
2024-09-12 23:03:39 +0530

    Day 33

--- Day33/missingNumber.java
+++ Day33/missingNumber.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class missingNumber {
-	
-	
+
 	public static void findMissingNumber(int[] arr) {
 		int n = arr.length+1;
-		
 		int sum1 = n*(n+1)/2;
 		int sum2 = 0;
 		for (int i = 0; i < arr.length; i++) {
 			sum2 +=arr[i];
 		}
-		
 		System.out.println(sum1-sum2);
 	}
+	 
+	
 	public static void main(String[] array) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
@@ -21,7 +20,6 @@ public class missingNumber {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i]=scan.nextInt();
 		}
-		
 		findMissingNumber(arr);
 	}
 }
