3eb04e6b6ea28d554381811d0b554f7dde0d02df
gunaasin <guna.asin06@gmail.com>
2024-09-17 13:03:21 +0530

    Encapsulation

--- Encapsulation/EncapsulationWithConstructor.java
+++ Encapsulation/EncapsulationWithConstructor.java
@@ -3,19 +3,23 @@ class Customer3{
 	private String cName;
 	private int cNum;
 	
-	public Customer3(int cId , String cName , int cNum) {
+	// constructor is setter.
+	public Customer3(int cId , String cName , int cNum) { 
+		// who's name same as class name , 
+		// who don't have a return type that is Constructor.
 		this.cId =cId;
 		this.cName = cName;
 		this.cNum = cNum;
 	}
-	
-	
+
 	public int getCustomerId() {
 		return cId;
 	}
+	
 	public String getCustomerName() {
 		return cName;
 	}
+	
 	public int getCustomerNum() {
 		return cNum;
 	}
@@ -26,7 +30,7 @@ class Customer3{
 public class EncapsulationWithConstructor {
 
 	public static void main(String[] args) {
-		Customer3 c = new Customer3(2112,"Guna",76757645);
+		Customer3 c = new Customer3(2112,"Guna",76757645); // Constructor call during Object creation
 		
 		
 		System.out.println(c.getCustomerId());
