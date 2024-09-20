c3c8ebdfcba38d5ce00cf208e3ca6a92a16e6c5c
gunaasin <guna.asin06@gmail.com>
2024-09-20 11:37:34 +0530

    Encapsulation Assignment Questions

---
+++ EncapsulationAssignmentQuestions/Question1.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+
+class Customer{
+	private int cId;
+	private String cName;
+	private String cEmail;
+	private long cNumber;
+	private String cPassword;
+	private String cAddress;
+	
+	
+	public void setCId(int cID) {
+		this.cId = cID;
+	}
+	public void setCName(String cName) {
+		this.cName = cName;
+	}
+	public void setCEmail(String cEmail) {
+		this.cEmail = cEmail;
+	}
+	public void setCNumber(long cNumber) {
+		this.cNumber = cNumber;
+	}
+	public void setCPassword(String cPassword) {
+		this.cPassword = cPassword;
+	}
+	public void setCAddress(String cAddress) {
+		this.cAddress = cAddress;
+	}
+	
+	
+	
+	public  int getCId() {
+		return cId;
+	}
+	public  String getCName() {
+		return cName;
+	}
+	public  String getCEmail() {
+		return cEmail;
+	}
+	public  long getCNumber() {
+		return cNumber;
+	}
+	public  String getCPassword() {
+		return cPassword;
+	}
+	public  String getCAddress() {
+		return  cAddress;
+	}
+	
+	
+}
+
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter Customer ID: ");
+		int cId = scan.nextInt();
+		System.out.println("Enter Customer Name: ");
+		scan.nextLine();
+		String cName = scan.nextLine();
+		System.out.println("Enter Customer Email: ");
+		String cEmail = scan.nextLine();
+		System.out.println("Enter Customer Number: ");
+		long cNum = scan.nextLong();
+		System.out.println("Enter Customer Password: ");
+		scan.nextLine();
+		String cPassword = scan.nextLine();
+		System.out.println("Enter Customer Address: ");
+		String cAddress = scan.nextLine();
+		
+		
+		Customer c = new Customer();
+		c.setCId(cId);
+		c.setCName(cName);
+		c.setCEmail(cEmail);
+		c.setCNumber(cNum);
+		c.setCPassword(cPassword);
+		c.setCAddress(cAddress);
+		
+		
+		System.out.println("Coustomer INFO !");
+		System.out.println();
+		System.out.println(c.getCId());
+		System.out.println(c.getCName());
+		System.out.println(c.getCEmail());
+		System.out.println(c.getCNumber());
+		System.out.println(c.getCPassword());
+		System.out.println(c.getCAddress());
+		
+		scan.close();
+	}
+
+}
---
+++ EncapsulationAssignmentQuestions/Question2.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+class Coustomer{
+	private int cId;
+	private String cName;
+	private String cEmail;
+	private long cNumber;
+	private String cPassword;
+	private String cAddress;
+	
+	public void setCId(int cID) {
+		this.cId = cID;
+	}
+	public void setCName(String cName) {
+		this.cName = cName;
+	}
+	public void setCEmail(String cEmail) {
+		this.cEmail = cEmail;
+	}
+	public void setCNumber(long cNumber) {
+		this.cNumber = cNumber;
+	}
+	public void setCPassword(String cPassword) {
+		this.cPassword = cPassword;
+	}
+	public void setCAddress(String cAddress) {
+		this.cAddress = cAddress;
+	}
+	
+	
+	
+	public  int getCId() {
+		return cId;
+	}
+	public  String getCName() {
+		return cName;
+	}
+	public  String getCEmail() {
+		return cEmail;
+	}
+	public  long getCNumber() {
+		return cNumber;
+	}
+	public  String getCPassword() {
+		return cPassword;
+	}
+	public  String getCAddress() {
+		return  cAddress;
+	}
+	
+	
+	
+}
+public class Question2 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter how many Coustomers are there : ");
+		int n = scan.nextInt();
+		Customer[] arr = new Customer[n];
+		
+		for(int i=0;i<n;i++) {
+			System.out.println("Enter Customer ID: ");
+			int cId = scan.nextInt();
+			System.out.println("Enter Customer Name: ");
+			scan.nextLine();
+			String cName = scan.nextLine();
+			System.out.println("Enter Customer Email: ");
+			String cEmail = scan.nextLine();
+			System.out.println("Enter Customer Number: ");
+			long cNum = scan.nextLong();
+			System.out.println("Enter Customer Password: ");
+			scan.nextLine();
+			String cPassword = scan.nextLine();
+			System.out.println("Enter Customer Address: ");
+			String cAddress = scan.nextLine();
+			
+			Customer c = new Customer();
+			c.setCId(cId);
+			c.setCName(cName);
+			c.setCEmail(cEmail);
+			c.setCNumber(cNum);
+			c.setCPassword(cPassword);
+			c.setCAddress(cAddress);
+			arr[i]=c;
+		}
+		
+		
+		for(Customer s:arr) {
+			System.out.println("Coustomer INFO !");
+			System.out.println();
+			
+			System.out.println(s.getCId());
+			System.out.println(s.getCName());
+			System.out.println(s.getCEmail());
+			System.out.println(s.getCNumber());
+			System.out.println(s.getCPassword());
+			System.out.println(s.getCAddress());
+		}
+		
+	}
+
+}
